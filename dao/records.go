dac56c2bfc71edd5eae050c824c003b38f74ed26
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-10-09 23:03:49 +0100

    Added json tags to record

--- dao/records.go
+++ dao/records.go
@@ -3,8 +3,8 @@ package dao
 import "github.com/izaakdale/service-delivery/model/delivery"
 
 type DeliveryRecord struct {
-	OrderId    string `dynamodbav:"PK"`
-	RecordType string `dynamodbav:"SK"`
-	Status     string
-	Address    *delivery.Address
+	OrderId    string            `dynamodbav:"PK" json:"order_id,omitempty"`
+	RecordType string            `dynamodbav:"SK" json:"record_type,omitempty"`
+	Status     string            `json:"status,omitempty"`
+	Address    *delivery.Address `json:"address,omitempty"`
 }
